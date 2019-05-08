21949ebee5f051435ba2619cfdf2404eef095230
Margot Konig <margot.konig@blackrock.com>
2019-05-08 16:06:54 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+	programmer_hash[:alan_kay][:known_for]
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,7 +79,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] => {
+     :known_for => "Ruby",
+     :languages => ["LISP, C"]
+ }
     
 end
 
@@ -101,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
 end
 
